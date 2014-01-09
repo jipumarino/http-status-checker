5252c528cde74c3120393e44e2db0377559f9cd5
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-01-09 09:05:25 -0300

    Sanitize urls

--- http-status-checker.rb
+++ http-status-checker.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'net/http'
 
-
 get '/' do
   erb :table
 end
 
 post '/' do
-  @responses = params[:urls].split("\n").map do |url|
+  urls = params[:urls].split("\n").map{|x| x.gsub(/^\W+/, '').gsub(/\W+$/, '')}.reject(&:empty?)
+  @responses = urls.map do |url|
     response = Net::HTTP.get_response(URI.parse(url))
     [url, response.code, response['location']]
   end
